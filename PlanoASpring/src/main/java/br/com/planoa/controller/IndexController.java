4f75ef789bd7e945f0e1e1784fceb4fa013fba13
Mª Antonieta <antonieta.paixao@yahoo.com>
2022-11-15 23:24:49 -0300

    Integração de links

--- PlanoASpring/src/main/java/br/com/planoa/controller/IndexController.java
+++ PlanoASpring/src/main/java/br/com/planoa/controller/IndexController.java
@@ -3,25 +3,46 @@ package br.com.planoa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-//@RequestMapping("/index")
+@RequestMapping("/")
 public class IndexController {
 
 
-	@GetMapping("/")
-	public String Index() { 
-		return "Index";
+	@GetMapping("/index")
+	public ModelAndView index() { 
+		ModelAndView modelAndView = new ModelAndView("/index.html");
+
+		return modelAndView;
 	}
 	
-	@GetMapping("/index")
-	public ModelAndView index() { // É chamada quando se tem "/cliente/cadastrar"
-		ModelAndView modelAndView = new ModelAndView("/index.html");
+	@GetMapping("/planosdeaula")
+	public ModelAndView planosdeaula() { 
+		ModelAndView modelAndView = new ModelAndView("/planosDeAula.html");
+		return modelAndView;
+	}
+	
+	@GetMapping("/perguntasfrequentes")
+	public ModelAndView perguntasfrequentes() { 
+		ModelAndView modelAndView = new ModelAndView("/perguntasFrequentes.html");
 		return modelAndView;
 	}
 
-		
+	@GetMapping("/sobrenos")
+	public ModelAndView sobrenos() { 
+		ModelAndView modelAndView = new ModelAndView("/sobreNos.html");
+		return modelAndView;
+	}
+
+	@GetMapping("/contato")
+	public ModelAndView contato() { 
+		ModelAndView modelAndView = new ModelAndView("/contato.html");
+		return modelAndView;
+	}
+
+
 }
 
